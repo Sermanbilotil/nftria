c60d5b23ab638717b1e80d78e87de9c91e9cedff
Serhii <bilotil.sergiy@gmail.com>
2022-08-15 17:30:09 +0300

    update react-moralis

--- config-overrides.js
+++ config-overrides.js
@@ -17,6 +17,7 @@ module.exports = function (config, env) {
                 http: require.resolve("stream-http"),
                 https: require.resolve("https-browserify"),
                 os: require.resolve("os-browserify"),
+                zlib: require.resolve("browserify-zlib"),
                 url: require.resolve("url"),
                 stream: require.resolve('stream-browserify'),
                 crypto: require.resolve("crypto-browserify")
